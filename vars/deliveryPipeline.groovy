1ee5d08d662f838889be34cff37dab48dcca28aa
git <claytnmorais@gmail.com>
2024-04-14 21:39:22 -0300

    ajuste no metodo da pipeline

--- vars/deliveryPipeline.groovy
+++ vars/deliveryPipeline.groovy
@@ -35,12 +35,9 @@ def call(body) {
         );
 
         println("revision: ${revision}")
-        kubernatesTools.deployK8s(parameters,"Mysql","mysql")
-        kubernatesTools.deployK8s(parameters,"Redis","redis")
-        kubernatesTools.deployK8s(parameters,"Zookeeper","kafka/zookeeper")
-        kubernatesTools.deployK8s(parameters,"Kafka Broker","kafka/kafka-broker")
-        kubernatesTools.deployK8s(parameters,"Kafka Broker","kafka/schema-registry")
-    }
+        params.package_manager.each{ stepName, stepDir ->  kubernatesTools.deployK8s(parameters,stepName,stepDir) }
+       
+     }
 
 
 }
